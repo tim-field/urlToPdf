edc7922f5b5c3c6ade8dc71f8baefad15759a832
Tim Field <tim@thefold.co.nz>
2017-05-24 09:00:23 +1200

    add search before print functionality

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(bodyParser.json())
 
 // Just to demonstrate the app working fetch on root of the app causes the PDF to be generated.
 app.post('/', (req, res) => {
-  const {url, delay} = req.body
-  urlToPdf({url, delay}).then((data) => {
+  const {url, delay, search} = req.body
+  urlToPdf({url, delay, search}).then((data) => {
     res.status(200).send(data)
   }).catch((err) => {
     console.error(err)
--- url-to-pdf.js
+++ url-to-pdf.js
@@ -5,19 +5,72 @@ const CHROME_PORT = 9222
 
 spawn(path.resolve(__dirname, 'chrome/headless_shell'), ['--no-sandbox', '--disable-gpu', `--remote-debugging-port=${CHROME_PORT}`])
 
-module.exports = ({url, delay = 500}) => {
-  return new Promise((resolve) => {
-    Chrome.New({port:CHROME_PORT}).then(() =>
-    Chrome((chromeInstance) => {
-      const { Page, Runtime } = chromeInstance;
-      Page.enable().then(()=> Page.navigate({url}))
-      Page.loadEventFired(() => setTimeout(() => {
-        Page.printToPDF().then((v) => {
-          resolve(v)
-          chromeInstance.close()
-        })
-      },delay))
+function tryQuery(DOM, docId, query, maxTries = 120) {
+  return new Promise((resolve, reject) =>
+    DOM.querySelector({nodeId: docId, selector: query})
+    .then(({nodeId}) => {
+      if (nodeId) {
+        resolve(nodeId)
+      }
+      else {
+        resolve(delay(500).then(() => tryQuery(DOM, docId, query, maxTries-1)))
+      }
+      if (maxTries < 1) {
+        reject(new Error('Can\'t find query selector: ',query))
+      }
     })
   )
-})
 }
+
+function delay(time) {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
+
+function printPDF(Page) {
+  // console.log('printing!')
+  return new Promise((resolve) => Page.printToPDF().then(resolve))
+}
+
+function onLoad(Page, delayTime) {
+  return new Promise((resolve) =>
+    Page.loadEventFired(() => {
+      if(delayTime) {
+        return delay(delayTime).then(resolve)
+      }
+      return resolve()
+    })
+  );
+}
+
+function onFound(DOM, search) {
+  return new Promise((resolve) => {
+    if(search) {
+      return DOM.getDocument()
+      .then((doc) => tryQuery(DOM, doc.root.nodeId, search))
+      .then(resolve)
+    } else {
+      return resolve()
+    }
+  })
+}
+
+module.exports = ({url, delay = 500, search = null}) => new Promise((resolve, reject) =>
+  Chrome.New({ port:CHROME_PORT })
+  .then(() => Chrome((chromeInstance) => {
+    const {Page, DOM} = chromeInstance
+    Page.enable()
+    .then(() => Page.navigate({url}))
+    .then(() => onLoad(Page, delay))
+    .then(() => onFound(DOM, search))
+    .then(() => printPDF(Page))
+    .then((pdf) => {
+      chromeInstance.close()
+      resolve(pdf)
+    })
+    .catch((err) => {
+      chromeInstance.close()
+      reject(err)
+    })
+  }
+  ))
+)
